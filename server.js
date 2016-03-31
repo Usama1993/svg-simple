08511f68927d9ffb3b0a69e529092f399db745cb
Usama Naseer <usaman1993@gmail.com>
2016-03-31 18:47:15 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var http=require('http');
+  fs = require('fs');
+  var port=Number(process.env.PORT || 3000);
+  
+  fs.readFile('./Task.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+
+http.createServer(function(request,response){
+	response.writeHeader(200,{'Content-Type':'text/html'});
+	response.write(html);
+response.end();}).listen(port);});
